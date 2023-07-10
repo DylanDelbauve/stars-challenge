6af6789b12a3d5d600dfd5ee69d9db3b978a36c0
Dylan D <dylandelbauve@gmail.com>
2023-07-10 17:00:50 +0200

    Adding final scoring + disable ts errors

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
+// @ts-nocheck
 import Rating from "./components/Rating.vue";
+import {ref} from "vue";
+
+const val1 = ref(0);
+const val2 = ref(0);
+const val3 = ref(0);
+const val4 = ref(0);
+
 </script>
 
 <template>
-    <rating></rating>
+    <div id="root">
+        <div>
+            <h2>Responsive</h2>
+            <rating @click="(v) => val1 = v"></rating>
+        </div>
+        <div>
+            <h2>Code</h2>
+            <rating @click="(v) => val2 = v"></rating>
+        </div>
+        <div>
+            <h2>Intégration</h2>
+            <rating @click="(v) => val3 = v"></rating>
+        </div>
+        <div>
+            <h2>Vertu</h2>
+            <rating @click="(v) => val4 = v"></rating>
+        </div>
+        <div v-show="val1 != 0 && val2 != 0 && val3 != 0 && val4 != 0">
+            <h2>Total</h2>
+            <rating :value="(val1+val2+val3+val4)/4"></rating>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    #root {
+        display: grid;
+        grid-column: 2;
+        grid-row: 2;
+    }
 </style>
--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,23 +1,55 @@
 <template>
     <div>
-        <svg @mousemove="hover(index)" @mouseout="out" @click.prevent="click(index)" class="star" :class="{clickAnimation: onClickAnimation}" ref="stars" v-for="index in 5"
-             width="64" height="64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
+        <div class="stars-result" ref="result" v-if="value != null && value != 0">
+            <svg :key="n" v-for="n in 5"
+                 width="64" height="64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+        </div>
+        <div class="stars">
+            <svg @mousemove="hover(index)" @mouseout="out" @click.prevent="click(index)" class="star" :class="{clickAnimation: onClickAnimation}" ref="stars" v-for="index in 5"
+                 width="64" height="64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+        </div>
+
     </div>
 </template>
 
 <script setup lang="ts">
+// @ts-nocheck
+const props = defineProps({
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    value: {
+        type: Number
+    }
+})
 
-import {Ref, ref} from "vue";
+const emit = defineEmits(['click'])
+
+import {onMounted, Ref, ref, watch} from "vue";
 
 const stars = ref([]);
+
+const result: Ref<HTMLElement> = ref(document);
 
 const rate: Ref<Number> = ref<Number>(0);
 
 const onClickAnimation = ref(false);
 
+onMounted(() => {
+    watch(() => props.value, (newValue) => {
+        if(newValue != null && newValue != 0 && result.value != null) {
+            result.value.style.maskImage = 'linear-gradient(90deg, #000000 0%, #000000 ' + newValue*20 + '%, transparent ' + newValue*20 + '%, transparent 100%)'
+            result.value.style.fill = `hsl(${newValue * 20}, 100%, 65%)`;
+        }
+    })
+})
 function hover(index: Number) {
     for (let i = 0; i < 5; i++) {
         stars.value[i].classList.remove('active', 'near');
@@ -26,7 +58,7 @@ function hover(index: Number) {
     for (let i = 0; i < index; i++) {
         let star = stars.value[i];
         star.classList.add('active');
-        star.style.fill = getColor(index);
+        star.style.fill = `hsl(${index * 20}, 100%, 65%)`;
     }
     if (index != 1)
         stars.value[index - 2].classList.add('near');
@@ -34,14 +66,16 @@ function hover(index: Number) {
 
 function click(index: number) {
     rate.value = index;
-    let delay = 300;
+    let delay = 20;
     stars.value.forEach((star, i) => {
         star.style.animationDelay = `${delay * i}ms`;
     });
     onClickAnimation.value = true
     setTimeout(() => {
         onClickAnimation.value = false
-    }, 2000)
+    }, 2000);
+
+    emit('click', rate.value)
 }
 
 function out() {
@@ -51,36 +85,15 @@ function out() {
     }
     for (let i = 0; i < rate.value; i++) {
         stars.value[i].classList.add('active');
-        stars.value[i].style.fill = getColor(rate.value);
+        stars.value[i].style.fill = `hsl(${rate.value * 20}, 100%, 65%)`;
 
     }
-}
-
-function getColor(index: number): string {
-    let color: string = "";
-    switch (index) {
-        case 1:
-            color = "rgb(242, 120, 83)";
-            break;
-        case 2:
-            color = "rgb(242,149,83)";
-            break;
-        case 3:
-            color = "rgb(242,205,83)";
-            break;
-        case 4:
-            color = "rgb(197,242,83)";
-            break;
-        case 5:
-            color = "rgb(147,242,83)";
-            break;
-    }
-    return color;
 }
 
 </script>
 
 <style scoped>
+
 .star {
     fill: rgba(83, 91, 242, 0.2);
     margin: 5px;
@@ -98,13 +111,28 @@ function getColor(index: number): string {
     transform: scale(110%) translateY(-5px);
 }
 
-div {
+.stars {
     display: flex;
+}
+.stars-result {
+    position: absolute;
+}
+
+.stars-result svg {
+    stroke: #1a1a1a;
+    stroke-width: .5;
+    margin: 5px;
 }
 
 .clickAnimation {
-    animation: 500ms linear 0s;
+    animation: 250ms linear 0s;
     animation-name: click;
+}
+.disabled {
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
 }
 
 @keyframes click {
